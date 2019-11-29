99cc060b16702f42c45f746334c4cab64fd97935
h662955 <h662955@stud.u-szeged.hu>
2019-11-29 14:40:49 +0100

    added toggle language option

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :dailies
   root to: 'pages#home'
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    # your routes here...
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
